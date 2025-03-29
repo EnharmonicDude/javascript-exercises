33da9845d72a0d5cf0dee406d3bf9d6b9ae84a76
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-03-29 11:36:52 +0100

    Solved 03_reverseString by turning string input into an array, reverse and join it and return the string value

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    //1. create a variable for the split string and equate it to the built-in method of .split, which splits a string into its compartments
+    let splitString = str.split("");
+
+    //2. create a variable for an array that equals the contents of the split string + the .reverse method
+    let reverseArray = splitString.reverse("");
+    
+    //3. create a variable for a joined array that joins all the reverse compartments of the string
+    let joinedArray = reverseArray.join("");
+
+
+    return joinedArray;
 };
 
 // Do not edit below this line
