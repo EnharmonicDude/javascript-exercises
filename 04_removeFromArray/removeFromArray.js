bf0f1ceafcc1d8c885f1ad03b6ff31f08382c9b3
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-03-29 16:36:25 +0100

    Had to look up the solution and comment it so I understand it for 04_removeFromArray because it was too difficult for me

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {//my new Array + random other arguments
+
+const newArray = []//1. create a new variable for my array
+
+array.forEach((item) => {//2. check each item in the array
+
+    if (!args.includes(item)){//3. if the item is NOT part of the function arguments
+        newArray.push(item);//4. add the item to the existing newArray
+    }
+}
+)
+
+return newArray; //5. and finally return the value of the updated newArray
+
+
 };
 
 // Do not edit below this line
