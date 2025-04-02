ca320d868764c42c0d901701cf45c2f45652eda3
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-02 18:09:33 +0200

    Solved exercise 06_leapYears but a bit more wordy than the solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+
+    //1. Receive a parameter which reflects a year number
+    //2. if the input is not an integer: return false
+    if (!Number.isInteger(year)){
+        return false;
+    }
+    //3. if the input is not divisible by 4, return false
+    else if (year % 4 !== 0){
+        return false;
+    }
+    else if (year % 4 === 0 && year % 100 !== 0){
+        return true;
+    }
+    else if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0){
+        return false;
+    }
+    else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0){
+        return true;
+    }
+    }
+;
 
 // Do not edit below this line
 module.exports = leapYears;
