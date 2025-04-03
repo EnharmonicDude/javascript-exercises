b4f4acbf789d496eb6de3f3b61c522eb779aa1c2
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-03 14:01:17 +0200

    started exercise 07_tempConversion by writing pseudocode

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
 const convertToCelsius = function() {
+
+  /*Receive a variable in Farenheit. Take the number and apply the formula(x − 32) × (5/9). Round this number to one decimal and return it + "°C".*/
 };
 
 const convertToFahrenheit = function() {
+
+  /*Receive a variable in Celsius. Take the number and apply the formula x × ⁠
+(9/5) + 32. Round this number to one decimal and return it + "°F". */
+
 };
 
 // Do not edit below this line
