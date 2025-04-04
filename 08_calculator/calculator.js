a3b93aa7c6571a9622d1f49393f4e14c78b95ee1
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-04 14:49:18 +0200

    Started 08_calculator exercise. Most of my comments are explanatory for myself and deliberately not 'clean' code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,35 @@
-const add = function() {
+const add = function(num1, num2) {
+
+  let result = (num1 + num2);
 	
+  return result;
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
 	
+  let result = (num1 - num2);
+	
+  return result;
+
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+  let sum = 0
+  for (let i = 0; i < arr.length; i++){
+
+    sum += arr[i]; //the sum is the addition of all instances of arr[i] which, of course, increase with each loop
+
+  }
+
+	return sum;
 };
 
 const multiply = function() {
+
+  let result = (num1 * num2);
+	
+  return result;
 
 };
 
