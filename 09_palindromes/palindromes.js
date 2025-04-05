95b25eda2ff2ac95177c77343b36926f97e92c01
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-05 11:54:52 +0200

    Solved 09_palindromes by making HEAVY use of sgtackoverflow and gemini explaining syntax to me

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^A-Za-z0-9]/g, "");//the last method removes empty spaces an all non-alphanumerical elements so it doesn't falsely generate an error
+  
+    for (let i = 0; i < string.length / 2; i++) {
+        //the string.length is being split into half because we want to check for the palindrome
+      if (string[i] !== string[string.length - 1 - i]) {//if the CONTENT of i DOES NOT equal the CONTENT of its mirror part (represented by string.length - 1 - i) then it is no palindrome
+        return false;
+      }
+    }
+  
+    return true;
+  };
 // Do not edit below this line
 module.exports = palindromes;
