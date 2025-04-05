3be0d8c987c4ea911ff05a41a21ee04d3f1a8e73
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-05 10:54:27 +0200

    Solved 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,20 +25,42 @@ const sum = function(arr) {
 	return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
 
-  let result = (num1 * num2);
-	
+  let product = 1
+  for (let i = 0; i < arr.length; i++){
+
+    product *= arr[i]; //the sum is the addition of all instances of arr[i] which, of course, increase with each loop
+
+  }
+  return product;
+
+};
+
+const power = function(num1, num2) {
+
+  //Take number 1 and multiply it by itself the number of times of number 2
+
+  let result = (num1 ** num2);
+
   return result;
-
-};
-
-const power = function() {
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    return "ERROR"; // Or some other error indicator
+  }
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let product = 1;//because if it is = 0 you get 0
+  for (let i = 1; i <= num; i++) {//start at 1 and increase every loop until you hit num.
+    product *= i; //first run (5): product = 1 * 1; second: product = 1 * 2; third: product = 2 * 3 etc
+  }
+  return product;
 };
 
 // Do not edit below this line
