af67f7369f490c663744afe9f5166280fe590d04
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-07 11:19:26 +0200

    Solved the 11_getTheTitles via array.<any>push()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+/*Receive an array as a parameter that contains two values: book title and book author. Iterate through the array and put the titles into a new array which you return. The array you receive will be of the form: 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]*/
+let titles = []
+
+for (let i = 0; i < books.length; i++){
+    titles.push(books[i].title); /*since in the array the property titles: is listed, you can reference it simply with this method. If it did not have the properties, you would code: for (let i = 0; i < books.length; i+=2){
+    titles.push(books[i])}*/
+}
+
+return titles;
 
 };
 
